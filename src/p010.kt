eaef82efba480b4a7b3f508682e5b2dc03e0710f
ainzzorl <ainzzorl@gmail.com>
2017-12-17 21:05:01 -0800

    Add solution to problem 10

---
+++ src/p010.kt
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/regular-expression-matching/description/
+class p010 {
+    fun isMatch(s: String, p: String): Boolean {
+        // match[i][j] = first i chars of s match the first j chars of p
+        val match = Array(s.length + 1, { BooleanArray(p.length + 1) })
+        match[0][0] = true
+        for (i in 1..s.length) {
+            match[i][0] = false
+        }
+        for (i in 1..p.length) {
+            if (p[i - 1] == '*') {
+                match[0][i] = match[0][i - 2]
+            } else {
+                match[0][i] = false
+            }
+        }
+        for (i in 1..s.length) {
+            for (j in 1..p.length) {
+                when {
+                    p[j - 1] == '*' -> {
+                        match[i][j] = false
+                        for (k in i downTo 0) {
+                            match[i][j] = match[i][j] || match[k][j - 2]
+                            if (k == 0 || !isMatch(s[k - 1], p[j - 2])) {
+                                break
+                            }
+                        }
+                    }
+                    else -> {
+                        match[i][j] = isMatch(s[i - 1], p[j - 1]) && match[i - 1][j - 1]
+                    }
+                }
+            }
+        }
+        return match[s.length][p.length]
+    }
+
+    private fun isMatch(c1: Char, c2: Char): Boolean {
+        return c1 == c2 || c1 == '.' || c2 == '.'
+    }
+}
---
+++ test/p010_test.kt
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+internal class p010_test {
+    private var solution: p010 = p010()
+
+    @Test
+    fun match() {
+        assertTrue(solution.isMatch("aa", "aa"))
+        assertTrue(solution.isMatch("aa", "a*"))
+        assertTrue(solution.isMatch("aa", ".*"))
+        assertTrue(solution.isMatch("ab", ".*"))
+        assertTrue(solution.isMatch("ab", "a.*b"))
+        assertTrue(solution.isMatch("aab", "c*a*b"))
+        assertTrue(solution.isMatch("aaa", "ab*ac*a"))
+    }
+
+    @Test
+    fun noMatch() {
+        assertFalse(solution.isMatch("aa", "a"))
+        assertFalse(solution.isMatch("aaa", "aa"))
+        assertFalse(solution.isMatch("aa", "aaa"))
+        assertFalse(solution.isMatch("a", "ab*a"))
+    }
+
+    @Test
+    fun edge() {
+        assertTrue(solution.isMatch("", ""))
+        assertFalse(solution.isMatch("a", ""))
+        assertFalse(solution.isMatch("", "a"))
+        assertTrue(solution.isMatch("", ".*"))
+        assertTrue(solution.isMatch("", "a*"))
+        assertTrue(solution.isMatch("", "a*a*"))
+        assertFalse(solution.isMatch("", "a*a*a"))
+    }
+}
