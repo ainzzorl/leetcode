3fefae2cf8fbe96ba59abbc4b933ab35309cffdb
ainzzorl <ainzzorl@gmail.com>
2017-12-22 18:23:55 -0800

    Add solution to problem 32

---
+++ src/p032.kt
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/longest-valid-parentheses/description/
+class p032 {
+    fun longestValidParentheses(s: String): Int {
+        var res = 0
+        for (left in 0 until s.length) {
+            var sum = 0
+            for (right in left until s.length) {
+                sum += if (s[right] == '(') 1 else -1
+                if (sum < 0) {
+                    break
+                }
+                if (sum == 0) {
+                    val len = right - left + 1
+                    res = if (len > res) len else res
+                }
+            }
+        }
+        return res
+    }
+}
---
+++ test/p032_test.kt
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class p032_test {
+    private var solution: p032 = p032()
+
+    @Test
+    fun test() {
+        assertEquals(2, solution.longestValidParentheses("(()"))
+        assertEquals(4, solution.longestValidParentheses(")()())"))
+        assertEquals(6, solution.longestValidParentheses("((()))"))
+        assertEquals(6, solution.longestValidParentheses("()()()"))
+        assertEquals(0, solution.longestValidParentheses(")))((("))
+        assertEquals(0, solution.longestValidParentheses(""))
+        assertEquals(2, solution.longestValidParentheses("()"))
+    }
+}
